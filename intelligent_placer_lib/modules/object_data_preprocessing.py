4abe653a8b0bf04b16ec610e828324a33d8c8d4b
katarinaaaaa <72103667+katarinaaaaa@users.noreply.github.com>
2022-10-23 00:12:48 +0300

    fixed comments in object_data_preprocessing.py

--- intelligent_placer_lib/modules/object_data_preprocessing.py
+++ intelligent_placer_lib/modules/object_data_preprocessing.py
@@ -12,22 +12,21 @@ from scipy import ndimage as ndi
 def get_mask_from_object_img(img_path: str):
     img = imread(img_path)
     img_blur_gray = rgb2gray(gaussian(img, sigma=1.5, channel_axis=True))
-    # бинаризация изображения
+    # Р±РёРЅР°СЂРёР·Р°С†РёСЏ РёР·РѕР±СЂР°Р¶РµРЅРёСЏ
     threshold_img = threshold_otsu(img_blur_gray)
     result_image = img_blur_gray <= threshold_img
-    # морфологические операции для улучшения полученной маски
+    # РјРѕСЂС„РѕР»РѕРіРёС‡РµСЃРєРёРµ РѕРїРµСЂР°С†РёРё РґР»СЏ СѓР»СѓС‡С€РµРЅРёСЏ РїРѕР»СѓС‡РµРЅРЅРѕР№ РјР°СЃРєРё
     result_image = binary_opening(result_image, footprint=np.ones((10, 10)))
     result_image = binary_closing(result_image, footprint=np.ones((50, 50)))
-    # попробуем отобрать нужную компоненту связности, используя то, что объект расположен в центре изображения
+    # РїРѕРїСЂРѕР±СѓРµРј РѕС‚РѕР±СЂР°С‚СЊ РЅСѓР¶РЅСѓСЋ РєРѕРјРїРѕРЅРµРЅС‚Сѓ СЃРІСЏР·РЅРѕСЃС‚Рё, РёСЃРїРѕР»СЊР·СѓСЏ С‚Рѕ, С‡С‚Рѕ РѕР±СЉРµРєС‚ СЂР°СЃРїРѕР»РѕР¶РµРЅ РІ С†РµРЅС‚СЂРµ РёР·РѕР±СЂР°Р¶РµРЅРёСЏ
     labels = measure.label(result_image)
     props = regionprops(labels)
-    # посмотрим на центроиды всех компонент связности
     center = (img.shape[0] / 2, img.shape[1] / 2)
-    # выбираем компоненту, центроид которой расположен наиболее близко к центру
+    # РІС‹Р±РёСЂР°РµРј РєРѕРјРїРѕРЅРµРЅС‚Сѓ, С†РµРЅС‚СЂРѕРёРґ РєРѕС‚РѕСЂРѕР№ СЂР°СЃРїРѕР»РѕР¶РµРЅ РЅР°РёР±РѕР»РµРµ Р±Р»РёР·РєРѕ Рє С†РµРЅС‚СЂСѓ
     dist = np.array([pow(center[0] - p.centroid[0], 2) + pow(center[1] - p.centroid[1], 2) for p in props])
     mask_id = dist.argmin()
     mask = (labels == (mask_id + 1))
-    # еще раз закроем дыры в маске
+    # РµС‰Рµ СЂР°Р· Р·Р°РєСЂРѕРµРј РґС‹СЂС‹ РІ РјР°СЃРєРµ
     mask = ndi.binary_fill_holes(mask)
     return mask
 
